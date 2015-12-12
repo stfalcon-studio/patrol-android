90cea35e7063ad949a03a2ad768386981dc4258b
Alex Zozulia <alexz89ua@gmail.com>
2015-12-12 16:59:56 +0200

    fix actions with video item in him lifecycle

--- app/src/main/java/com/stfalcon/hromadskyipatrol/utils/StringUtilities.java
+++ app/src/main/java/com/stfalcon/hromadskyipatrol/utils/StringUtilities.java
@@ -10,14 +10,17 @@ import java.util.ArrayList;
  * Created by troy379 on 06.11.15.
  */
 public final class StringUtilities {
-    private StringUtilities() { throw new AssertionError(); }
+    private StringUtilities() {
+        throw new AssertionError();
+    }
 
-    public static ArrayList<String> getOptions(Context context, boolean isCanLoad) {
+    public static ArrayList<String> getOptions(Context context, boolean isCanLoad,
+                                               boolean isCanDelete) {
         ArrayList<String> options = new ArrayList<>();
 
         options.add(context.getString(R.string.view_video));
         if (isCanLoad) options.add(context.getString(R.string.upload_video));
-        options.add(context.getString(R.string.delete_video));
+        if (isCanDelete) options.add(context.getString(R.string.delete_video));
 
         return options;
     }
