88c23a5c3e460f66a784e6aea971e14471b4e49d
Alex Zozulia <alexz89ua@gmail.com>
2016-07-06 15:41:00 +0300

    add license

--- app/src/main/java/com/stfalcon/hromadskyipatrol/camera/fragment/Camera2VideoFragment.java
+++ app/src/main/java/com/stfalcon/hromadskyipatrol/camera/fragment/Camera2VideoFragment.java
@@ -1,17 +1,21 @@
 /*
- * Copyright 2014 The Android Open Source Project
+ * Copyright (c) 2015 - 2016. Stepan Tanasiychuk
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *     This file is part of Gromadskyi Patrul is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Found ation, version 3 of the License, or any later version.
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *     If you would like to use any part of this project for commercial purposes, please contact us
+ *     for negotiating licensing terms and getting permission for commercial use.
+ *     Our email address: info@stfalcon.com
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.stfalcon.hromadskyipatrol.camera.fragment;
--- app/src/main/java/com/stfalcon/hromadskyipatrol/services/UploadService.java
+++ app/src/main/java/com/stfalcon/hromadskyipatrol/services/UploadService.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2015 - 2016. Stepan Tanasiychuk
+ *
+ *     This file is part of Gromadskyi Patrul is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Found ation, version 3 of the License, or any later version.
+ *
+ *     If you would like to use any part of this project for commercial purposes, please contact us
+ *     for negotiating licensing terms and getting permission for commercial use.
+ *     Our email address: info@stfalcon.com
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.stfalcon.hromadskyipatrol.services;
 
 import android.app.IntentService;
